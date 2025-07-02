afbef98e13b31651cc0b09b328ab1435c5a36e14
atari <atari@users.noreply.github.com>
2025-07-02 10:43:17 +0200

    Update ip2.sh

--- ip2.sh
+++ ip2.sh
@@ -4,10 +4,10 @@ mkdir ip
 echo -e "\033[0;30m"
 conntrack -L -p udp -f ipv4 | grep ASSURED  > 1
 conntrack -L -p udp -f ipv6 | grep ASSURED > 1v6
-cat 1 | awk '{ print $4 }' | awk -F= '{ print $2 }' | sort | uniq -c | awk '{ print $2 }' | sed 's/\.[0-9]*$/.0/' > 2
-cat 1v6 | awk '{ print $5 }' | awk -F= '{ print $2 }' | sort | uniq -c | awk '{ print $2 }' | awk '{if(NF<8){inner = "0"; for(missing = (8 - NF);missing>0;--missing){inner = inner ":0"}; if($2 == ""){$2 = inner} else if($3 == ""){$3 = inner} else if($4 == ""){$4 = inner} else if($5 == ""){$5 = inner} else if($6 == ""){$6 = inner} else if($7 == ""){$7 = inner}}; print $0}' FS=":" OFS=":" | awk '{for(i=1;i<9;++i){len = length($(i)); if(len < 1){$(i) = "0000"} else if(len < 2){$(i) = "000" $(i)} else if(len < 3){$(i) = "00" $(i)} else if(len < 4){$(i) = "0" $(i)} }; print $0}' FS=":" OFS=":" | grep -o ^....:....: | sed 's/:$/::/' >> 2
+cat 1 | awk '{ print $4 }' | awk -F= '{ print $2 }' | sort -u | sed 's/\.[0-9]*$/.0/' > 2
+cat 1v6 | awk '{ print $5 }' | awk -F= '{ print $2 }' | sort -u | awk '{if(NF<8){inner = "0"; for(missing = (8 - NF);missing>0;--missing){inner = inner ":0"}; if($2 == ""){$2 = inner} else if($3 == ""){$3 = inner} else if($4 == ""){$4 = inner} else if($5 == ""){$5 = inner} else if($6 == ""){$6 = inner} else if($7 == ""){$7 = inner}}; print $0}' FS=":" OFS=":" | awk '{for(i=1;i<9;++i){len = length($(i)); if(len < 1){$(i) = "0000"} else if(len < 2){$(i) = "000" $(i)} else if(len < 3){$(i) = "00" $(i)} else if(len < 4){$(i) = "0" $(i)} }; print $0}' FS=":" OFS=":" | grep -o ^....:....: | sed 's/:$/::/' >> 2
 Linenum=`wc -l < 2`
-if [ "$Linenum" -lt 2396 ];
+if [ "$Linenum" -lt 5396 ];
 then
 split -l 99 -d 2 ip/3
 else
