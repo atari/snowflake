f88b5b7daa9385a02ab3413e2b9fb7cb4e404c5e
atari <atari@users.noreply.github.com>
2025-07-02 08:34:00 +0200

    Update ip2.sh
    Separating ipv4 and ipv6. Truncating ipv6 like ipv4 (for privacy)

--- ip2.sh
+++ ip2.sh
@@ -2,11 +2,12 @@
 # set -x
 mkdir ip
 echo -e "\033[0;30m"
-conntrack -L -p udp | grep ASSURED  > 1
-conntrack -L -p udp -f ipv6 | grep ASSURED >> 1
+conntrack -L -p udp -f ipv4 | grep ASSURED  > 1
+conntrack -L -p udp -f ipv6 | grep ASSURED > 1v6
 cat 1 | awk '{ print $4 }' | awk -F= '{ print $2 }' | sort | uniq -c | awk '{ print $2 }' | sed 's/\.[0-9]*$/.0/' > 2
+cat 1v6 | awk '{ print $5 }' | awk -F= '{ print $2 }' | sort | uniq -c | awk '{ print $2 }' | awk '{if(NF<8){inner = "0"; for(missing = (8 - NF);missing>0;--missing){inner = inner ":0"}; if($2 == ""){$2 = inner} else if($3 == ""){$3 = inner} else if($4 == ""){$4 = inner} else if($5 == ""){$5 = inner} else if($6 == ""){$6 = inner} else if($7 == ""){$7 = inner}}; print $0}' FS=":" OFS=":" | awk '{for(i=1;i<9;++i){len = length($(i)); if(len < 1){$(i) = "0000"} else if(len < 2){$(i) = "000" $(i)} else if(len < 3){$(i) = "00" $(i)} else if(len < 4){$(i) = "0" $(i)} }; print $0}' FS=":" OFS=":" | grep -o ^....:....: | sed 's/:$/::/' >> 2
 Linenum=`wc -l < 2`
-if [ "$Linenum" -lt 396 ];
+if [ "$Linenum" -lt 2396 ];
 then
 split -l 99 -d 2 ip/3
 else
@@ -24,5 +25,5 @@ done;
 echo -e "\033[1;37m"
 cat 3 | sed 's/{country://g' | sed 's/}//;s/{//g' | sed 's/]//g' | sed 's/\[//g' | sort | uniq -c
 echo -e "\033[0m"
-/bin/rm -r -f 1 2 3 2-300 2-301 2-302 2-303
+/bin/rm -r -f iv6 1 2 3 2-30*
 /bin/rm -r ip
